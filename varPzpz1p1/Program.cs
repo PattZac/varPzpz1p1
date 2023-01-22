57e3d949526ba5d05c306063cdb588e0b6d9aba0
Pat <113007904+PattZac@users.noreply.github.com>
2023-01-22 15:12:05 +0100

    Add files via upload

---
+++ varPzpz1p1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace varPzpz1p1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, d;
+            Console.WriteLine("Unesite a: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Unesite b: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Unesite c: ");
+            c = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Unesite d: ");
+            d = Convert.ToDouble(Console.ReadLine());
+            double x = (a + b) * 2 + (c + d) * 2;
+            Console.WriteLine("x je " + x + ".");
+            Console.ReadLine();
+        }
+    }
+}
